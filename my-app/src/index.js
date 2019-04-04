0e3aac1d6229e490caacf9d9aaaf6a70f59272b3
Luke <lukebulosan@gmail.com>
2019-04-04 11:47:49 -1000

    V1

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AkamaiNook extends React.Component {
+
+  render() {
+    return (
+        <div>
+        <h1>Akamai Nook</h1>
+        </div>
+  );
+  }
+}
+
+ReactDOM.render(<AkamaiNook />, document.getElementById('root'));
