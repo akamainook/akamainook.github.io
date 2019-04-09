4cc68e3044462b176b19a5cb425a977409bfb1f3
Luke <lukebulosan@gmail.com>
2019-04-09 13:05:12 -1000

    Issue 9 Completed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './style.css'
-import { Container, Header, Image, Menu, Icon, Dropdown, Grid, Divider, List, Input, Item, Button } from 'semantic-ui-react';
-
+import {
+  Container,
+  Header,
+  Image,
+  Menu,
+  Icon,
+  Dropdown,
+  Grid,
+  Divider,
+  List,
+  Input,
+  Item,
+  Button,
+  Search,
+  Embed
+} from 'semantic-ui-react';
 
 class TopMenu extends React.Component {
   render() {
@@ -23,14 +37,27 @@ class TopMenu extends React.Component {
 class MiddleHome extends React.Component {
   render() {
     return (
-        <Grid columns={2} divided>
-            <Grid.Column>
-              <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
-            </Grid.Column>
-            <Grid.Column>
-              <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
-            </Grid.Column>
-        </Grid>
+
+        <div className="main-background">
+          <div className="center-class">
+            <Container>
+              <Grid columns={2}>
+                <Grid.Column>
+                  <Embed
+                      defaultActive='true'
+                      url='https://www.google.com/maps/embed/v1/place?q=place_id:ChIJVdiAlZhtAHwR9NKOg97yY6w&key=AIzaSyCRDPjPd6VNBZNTkiJ9Ad46v3t7BYEAymg'
+                  />
+                </Grid.Column>
+                <Grid.Column textAlign="centered">
+                  <div className="center-class">
+                    <Input fluid icon='search' placeholder='Search...'/>
+                  </div>
+                </Grid.Column>
+              </Grid>
+            </Container>
+          </div>
+        </div>
+
     )
   }
 }
@@ -42,14 +69,15 @@ class FooterMenu extends React.Component {
           <Grid container columns={2}>
             <Grid.Column>About Akamai Nook
               <hr/>
-                <text>Akamai Nook is a team of college students who want
-                 to provide a simple and efficient way to find and share
-                study spots on Oahu.</text>
+              <text>Akamai Nook is a team of college students who want
+                to provide a simple and efficient way to find and share
+                study spots on Oahu.
+              </text>
             </Grid.Column>
             <Grid.Column>Links
               <hr/>
               <Button icon>Our Github Page
-                <Icon huge name='github square' />
+                <Icon huge name='github square'/>
               </Button>
             </Grid.Column>
           </Grid>
@@ -63,12 +91,12 @@ class AkamaiNook extends React.Component {
   render() {
     return (
         <div>
-        <TopMenu/>
-        <MiddleHome/>
-        <FooterMenu/>
+          <TopMenu/>
+          <MiddleHome/>
+          <FooterMenu/>
         </div>
-  );
+    );
   }
 }
 
-ReactDOM.render(<AkamaiNook />, document.getElementById('root'));
+ReactDOM.render(<AkamaiNook/>, document.getElementById('root'));
