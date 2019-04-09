2cb8c7de7df94c08efe4a54ab653f6b18a13cc53
Luke <lukebulosan@gmail.com>
2019-04-09 11:19:54 -1000

    Navbar and Footer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import './style.css'
+import { Container, Header, Image, Menu, Icon, Dropdown, Grid, Divider, List, Input, Item, Button } from 'semantic-ui-react';
+
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="topmenu">
+          <Container>
+            <Menu.Item><h1>Akamai Nook</h1></Menu.Item>
+            <Menu.Item position="right">HOME</Menu.Item>
+            <Menu.Item>ADD A SPOT</Menu.Item>
+            <Menu.Item>PROFILE</Menu.Item>
+          </Container>
+        </Menu>
+    )
+  }
+}
+
+class MiddleHome extends React.Component {
+  render() {
+    return (
+        <Grid columns={2} divided>
+            <Grid.Column>
+              <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+              <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+        </Grid>
+    )
+  }
+}
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+        <Container fluid className="footer-background">
+          <Grid container columns={2}>
+            <Grid.Column>About Akamai Nook
+              <hr/>
+                <text>Akamai Nook is a team of college students who want
+                 to provide a simple and efficient way to find and share
+                study spots on Oahu.</text>
+            </Grid.Column>
+            <Grid.Column>Links
+              <hr/>
+              <Button icon>Our Github Page
+                <Icon huge name='github square' />
+              </Button>
+            </Grid.Column>
+          </Grid>
+        </Container>
+    )
+  }
+}
 
 class AkamaiNook extends React.Component {
 
   render() {
     return (
         <div>
-        <h1>Akamai Nook</h1>
+        <TopMenu/>
+        <MiddleHome/>
+        <FooterMenu/>
         </div>
   );
   }
